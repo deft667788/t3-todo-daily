fdb5bbf8fb8ee86b6d43d73612568552ead91c78
Hanwen Shi <hanwen.shi667788@gmail.com>
2023-06-20 23:38:36 +1000

    feat: responsive

--- src/components/home/Nav.tsx
+++ src/components/home/Nav.tsx
@@ -4,7 +4,7 @@ import Icon from "../../../public/icon.svg";
 
 export default function Nav() {
 	return (
-		<div className="flex justify-between items-center h-24 px-27">
+		<div className="flex justify-between items-center h-24 px-2 sm:px-27">
           <div className="flex items-center gap-x-2">
             <Image src={Icon as string} alt="logo" />
             <span className="text-accent-red font-bold">Todo Daily</span>
