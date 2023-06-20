b1efadfe12d7031fa21787ec890dcefb12106555
Hanwen Shi <hanwen.shi667788@gmail.com>
2023-06-20 20:26:22 +1000

    fix: debug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,6 +12,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      { process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} /> }
     </SessionProvider>
   );
 };
